24386ca528814c7fee177254e1b271df87bc6c4d
Ventsislav-Ignatov <vencislav.ignatov@gmail.com>
2020-11-16 20:11:38 +0200

    Add View for NotOwner and add InputModels

---
+++ src/Web/MountainSocialNetwork.Web.ViewModels/Articles/ArticlePostCreateInputModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MountainSocialNetwork.Web.ViewModels.BlogPosts
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Ganss.XSS;
+    using Microsoft.AspNetCore.Http;
+
+    public class ArticlePostCreateInputModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Should be minimum five symbols!")]
+        [RegularExpression("[А-Я]+", ErrorMessage = "Name should start with upper letter.")]
+        [Display(Name = "Title")]
+        [MinLength(5)]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        [MinLength(200, ErrorMessage = "Short content! Must be more than 200 symbols!")]
+        public string Content { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public ICollection<IFormFile> Picture { get; set; }
+
+        public IEnumerable<CategoryDropDownViewModel> Categories { get; set; }
+
+    }
+}
---
+++ src/Web/MountainSocialNetwork.Web.ViewModels/NewsFeed/DeletePostInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MountainSocialNetwork.Web.ViewModels.NewsFeed
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    using MountainSocialNetwork.Data.Models;
+    using MountainSocialNetwork.Services.Mapping;
+
+    public class DeletePostInputModel : IMapFrom<NewsFeedPost>
+    {
+        [Required]
+        public int Id { get; set; }
+
+    }
+}
